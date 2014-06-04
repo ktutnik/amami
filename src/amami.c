026f924552413414a6e0aec797b5f6e7a75dc388
ktutnik <ktutnik@gmail.com>
2014-06-04 16:17:27 -0700

    adding charging info

--- src/amami.c
+++ src/amami.c
@@ -12,13 +12,32 @@ TextLayer *minute_two_layer;
 TextLayer *info_layer;
 TextLayer *battery_layer;
 TextLayer *date_layer;
+TextLayer *charging_layer;
 bool is_in_detail_face = false;
+
+static void battery_callback(BatteryChargeState state){
+  if(state.is_charging){
+    static char battery_text[] = "Mengisi 100%";
+    layer_set_hidden(text_layer_get_layer(charging_layer), false);
+    snprintf(battery_text, sizeof(battery_text), "Mengisi %d%%", state.charge_percent);
+    text_layer_set_text(charging_layer, battery_text);
+  }
+  if(state.is_plugged & !state.is_charging){
+    layer_set_hidden(text_layer_get_layer(charging_layer), false);
+    text_layer_set_text(charging_layer, "Penuh");
+  }
+  else {
+    layer_set_hidden(text_layer_get_layer(charging_layer), true);
+  }
+}
 
 static void show_normal_face(){
   Layer *root_layer = window_get_root_layer(window);
   GRect frame = layer_get_frame(root_layer);
   layer_set_bounds(text_layer_get_layer(time_layer), GRect(0, 20, frame.size.w, frame.size.h));
   layer_set_hidden(text_layer_get_layer(info_layer), true);
+  //tampilkan charging kalo lagi ngecas
+  battery_callback(battery_state_service_peek());
 }
 
 static void show_detail_face(){
@@ -43,7 +62,11 @@ static void show_detail_face(){
   strftime(date_text, sizeof(date_text), " %e", tick_time);
   strcat(day_text, date_text);
   text_layer_set_text(date_layer, day_text);
+  
+  //sembunyikan charging kalo lagi ngecas
+  layer_set_hidden(text_layer_get_layer(charging_layer), true);
 }
+
 
 static void timer_callback(void *context) {
   is_in_detail_face = false;
@@ -92,6 +115,7 @@ static void do_init(void) {
   Layer *root_layer = window_get_root_layer(window);
   GRect frame = layer_get_frame(root_layer);
   
+  charging_layer = my_create_layer(GRect(0, 0, frame.size.w - 2, 26), FONT_KEY_ROBOTO_CONDENSED_21, GTextAlignmentCenter, GColorWhite, GColorClear);
   hour_layer = my_create_layer(GRect(0, 0, frame.size.w, 50), FONT_KEY_BITHAM_42_BOLD, GAlignLeft, GColorWhite, GColorClear);
   minute_one_layer = my_create_layer(GRect(0, 38, frame.size.w, 50), FONT_KEY_BITHAM_42_LIGHT, GAlignLeft, GColorWhite, GColorClear);
   minute_two_layer = my_create_layer(GRect(0, 75, frame.size.w, 50), FONT_KEY_BITHAM_42_LIGHT, GAlignLeft, GColorWhite, GColorClear);
@@ -108,6 +132,9 @@ static void do_init(void) {
 
   tick_timer_service_subscribe(MINUTE_UNIT, &handle_second_tick);
   accel_tap_service_subscribe(&accel_tap_handler);
+  battery_state_service_subscribe(&battery_callback);
+  
+  layer_add_child(root_layer, text_layer_get_layer(charging_layer));
   
   info_layer = text_layer_create(GRect(0, 120, frame.size.w, 50));
   text_layer_set_background_color(info_layer, GColorClear);
@@ -123,11 +150,13 @@ static void do_init(void) {
   layer_add_child(root_layer, text_layer_get_layer(time_layer));
   
   show_normal_face();
+  
 }
 
 static void do_deinit(void) {
   tick_timer_service_unsubscribe();
   accel_tap_service_unsubscribe();
+  battery_state_service_unsubscribe();
   
   text_layer_destroy(hour_layer);
   text_layer_destroy(minute_one_layer);
@@ -136,6 +165,7 @@ static void do_deinit(void) {
   text_layer_destroy(date_layer);
   text_layer_destroy(time_layer);
   text_layer_destroy(info_layer);
+  text_layer_destroy(charging_layer);
   window_destroy(window);
 }
 
